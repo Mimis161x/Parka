e8b460fa2f3e6f102cfc3e5815c26f60027ac558
Mimis161x <146423932+Mimis161x@users.noreply.github.com>
2026-01-31 15:12:55 +0200

    fιχ: set up dot env configurations so the server now is running correctly in the desired port

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,13 @@
 import app from './app';
 import db from './utils/db';
+import dotenv  from "dotenv";
 
-const port = process.env.PORT || 3000;
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 const start = async () => {
     await db();
-    app.listen(PORT, () => {console.log(`Server started on http://localhost${port}`)});
+    app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`));
 }
-
 
 start();
