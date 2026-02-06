08ad5f3431b05fa50303354a70f3037521b90943
Mimis161x <146423932+Mimis161x@users.noreply.github.com>
2026-02-06 23:11:18 +0200

    feat: initialized frontend

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Parka
 A parking reservation full-stack web app
+
+Τρέξτε το docker στον υπολογιστή σας και ύστερα την εντολή docker-compose up --build
+
+Γνωρίζω οτι το σάιτ είναι ελλιπές αναφορικά με κάποιες λειτουργικότητες αλλά σκοπεύω να το αναβαθμίσω μόλις μου δώσετε 
+και εσείς το σχετικό input.
+
+Επίσης σκοπεύω να βάλω stripe για πληρωμές και google maps api για τις τοποθεσίες, καθώς και google api για log in με gmail
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,13 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     server: {
+        host: true,
+        strictPort: true,
         port: 5173,
-        proxy: {
-            '/api': {
-                target: 'http://localhost:4000',
-                changeOrigin: true,
-                secure: false,
-            },
+        watch: {
+            usePolling: true,
         },
     },
 })
